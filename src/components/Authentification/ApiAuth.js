5bbeed606cc428ff52e3f60a8adf55fddd7907dd
Vadim Batin <vadim.batin@stefanini.com>
2023-09-21 18:07:56 +0300

    fix: Auth

--- src/components/Authentification/ApiAuth.js
+++ src/components/Authentification/ApiAuth.js
@@ -1,9 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {build} from "vite";
+// import {build} from "vite"; Wrong inport delete
 
 export const apiAuth = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "http://127.0.0.1:5000"
+        // use prepareHeaders to set the auth token https://redux-toolkit.js.org/rtk-query/api/fetchBaseQuery#prepareheaders
+        // Check refresh token example https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#automatic-re-authorization-by-extending-fetchbasequery
     }),
     endpoints: builder => ({
         createUser: build.mutation({
